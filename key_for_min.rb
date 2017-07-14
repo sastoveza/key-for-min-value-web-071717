f7cf6af4a5326e789dbb92d1708850c03aee21bf
Stacey Astoveza <staceyastoveza@transfix.io>
2017-07-14 16:42:30 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  prev = ""
+  if !name_hash.empty?
+    min = name_hash.first.last
+  end
+  name_hash.each do |key, value|
+    if value < min
+      min = value
+    end
+    prev = value
+  end
 
+  name_hash.key(min)
 end
